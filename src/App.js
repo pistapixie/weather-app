8fa30d55187c3b5e534cb5ee04507aa9481fe80c
pistapixie <hleehslee@gmail.com>
2024-03-24 01:29:08 +0900

    [Feat]city name 리스트 랜더링

--- src/App.js
+++ src/App.js
@@ -13,13 +13,14 @@ import WeatherButton from "./component/WeatherButton";
 
 function App() {
   let [weather, setWeather] = useState(null);
+  const cities = ["Paris", "New York", "London"];
   const getCurrentLocation = () => {
     // console.log("get");
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
       getWeatherByCurrentLocation(lat, lon);
-      console.log("현재위치", lat, lon);
+      // console.log("현재위치", lat, lon);
     });
   };
 
@@ -27,7 +28,7 @@ function App() {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=02e0d489d7afe475b33c2fa298cb5a22&units=metric`;
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
+    // console.log(data);
     setWeather(data);
   };
 
@@ -39,7 +40,7 @@ function App() {
     <div>
       <div className="container">
         <WeatherBox weather={weather} />
-        <WeatherButton />
+        <WeatherButton cities={cities} />
       </div>
     </div>
   );
--- src/component/WeatherButton.jsx
+++ src/component/WeatherButton.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const WeatherButton = () => {
+const WeatherButton = ({ cities }) => {
+  // console.log(cities);
   return (
-    <div>
+    <div className="button-container">
       <Button variant="warning">Current Location</Button>
-      <Button variant="warning">Lodon</Button>
-      <Button variant="warning">Paris</Button>
+      {cities.map((item) => (
+        <Button variant="warning">{item}</Button>
+      ))}
     </div>
   );
 };
