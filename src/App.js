11ebb0287411649e0b9a3e60c3de8fd96cae6069
pistapixie <hleehslee@gmail.com>
2024-03-25 01:30:49 +0900

    [Feat]로딩 스피너 구현

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import WeatherBox from "./component/WeatherBox";
 import WeatherButton from "./component/WeatherButton";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
-  let [weather, setWeather] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
   const cities = ["Paris", "New York", "London"];
 
   useEffect(() => {
@@ -19,20 +22,49 @@ function App() {
 
     const getWeatherByCurrentLocation = async (lat, lon) => {
       let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=02e0d489d7afe475b33c2fa298cb5a22&units=metric`;
+      setLoading(true);
       let response = await fetch(url);
       let data = await response.json();
       setWeather(data);
+      setLoading(false);
     };
 
     getCurrentLocation();
   }, []);
 
+  useEffect(() => {
+    const getWeatherByCity = async () => {
+      if (city !== "") {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=02e0d489d7afe475b33c2fa298cb5a22&units=metric`;
+        setLoading(true);
+        let response = await fetch(url);
+        let data = await response.json();
+        setWeather(data);
+        setLoading(false);
+      }
+    };
+
+    getWeatherByCity();
+  }, [city]);
+
   return (
     <div>
-      <div className="container">
-        <WeatherBox weather={weather} />
-        <WeatherButton cities={cities} />
-      </div>
+      {loading ? (
+        <div className="container">
+          <ClipLoader
+            color="#f88c6b"
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div className="container">
+          <WeatherBox weather={weather} />
+          <WeatherButton cities={cities} setCity={setCity} />
+        </div>
+      )}
     </div>
   );
 }
--- src/component/WeatherButton.jsx
+++ src/component/WeatherButton.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const WeatherButton = ({ cities }) => {
+const WeatherButton = ({ cities, setCity }) => {
   // console.log(cities);
   return (
     <div className="button-container">
       <Button variant="warning">Current Location</Button>
-      {cities.map((item) => (
-        <Button key={item} variant="warning">
+      {cities.map((item, index) => (
+        <Button key={index} variant="warning" onClick={() => setCity(item)}>
           {item}
         </Button>
       ))}
